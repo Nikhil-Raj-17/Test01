a4eebc038319895e843711fcb8b65c4fba904ac0
Nikhil-Raj-17 <nikhilraj1708@gmail.com>
2025-03-25 16:56:00 +0530

    Update Sign_in.java

--- app/src/main/java/com/example/test01/Sign_in.java
+++ app/src/main/java/com/example/test01/Sign_in.java
@@ -1,6 +1,9 @@
 package com.example.test01;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,25 @@ public class Sign_in extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button signInButton = findViewById(R.id.signin);
+        Button signUpButton = findViewById(R.id.Signup);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Sign_in.this, signing_in.class);
+                startActivity(intent);
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Sign_in.this, Singing_up.class);
+                startActivity(intent);
+            }
         });
     }
 }
